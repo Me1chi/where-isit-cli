74b7f1ac51e63dea5e8d55c6a960491034984fa7
Melchior Boaretto Neto <melchibneto@gmail.com>
2025-03-06 03:20:54 -0300

    Fighting the borrow checker

---
+++ README.md
@@ -0,0 +1,8 @@
+I study in a city far from home.
+
+This application is meant to help me keep track of where my things are.
+
+You're supposed to choose the kind of location (e.g. Cities, Houses, Places in bedroom, etc...).
+It will be prompted every launch until you choose a kind.
+
+The current location of the item will be printed in green, among the others in gray.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+use colored::Colorize;
+use std::io;
+
+struct Item<'a> {
+    item_name: String,
+    possible_locales: &'a Vec<String>,
+    locale_index: usize,
+}
+
+impl<'a> Item<'a> {
+    fn find_index_locale(&mut self, locale_to_find: String) {
+        for (index, locale) in self.possible_locales.iter().enumerate() {
+            if locale_to_find.eq(locale) {
+                self.locale_index = index;
+            }
+        }
+    }
+}
+
+struct Collection<'a> {
+    coll_name: String,
+    items: Vec<Item<'a>>,
+}
+
 fn main() {
-    println!("Brazil above all, God above the people!");
+    //the locales and collections
+    let mut user_locales: Vec<String> = Vec::new();
+    let mut user_collections: Vec<Collection> = Vec::new();
+
+    //TESTS BELOW
+    user_locales.push(String::from("Quarto"));
+    user_locales.push(String::from("Sala"));
+    user_locales.push(String::from("Cozinha"));
+
+    let item_one = Item {
+        item_name: String::from("Nirvana"),
+        possible_locales: &user_locales,
+        locale_index: 0,
+    };
+
+    let item_two = Item {
+        item_name: String::from("Metallica"),
+        possible_locales: &user_locales,
+        locale_index: 0,
+    };
+
+    let item_three = Item {
+        item_name: String::from("AC/DC"),
+        possible_locales: &user_locales,
+        locale_index: 0,
+    };
+
+    let my_collection = Collection {
+        coll_name: String::from("discos"),
+        items: vec![item_one, item_two, item_three],
+    };
+
+    user_collections.push(my_collection);
+
+    println!("This is my {} collection: ", user_collections[0].coll_name);
+
+    for (index, item) in user_collections[0].items.iter().enumerate() {
+        println!("{}: {}", index + 1, item.item_name);
+    }
+    //TESTS ENDING
 }
